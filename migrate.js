d683cc1048f3668d7d3f0d332824f013debd7e9f
Andry <andreymahdison@gmail.com>
2018-11-18 00:21:10 +0700

    refactor

--- migrate.js
+++ migrate.js
@@ -1,46 +1,83 @@
 'use strict';
 
 const Excel = require('exceljs');
-const mysql = require('mysql');
+const path = require('path');
+const fs = require('fs');
 const Model = require('./model/migrator_object');
 
-const migrateFilePath = './data/MasterLokasiUjianOnlineNonReguler.xlsx';
-const documentRowStart = 6;
+global.response = {};
 
-var workbook = new Excel.Workbook();
-const records = [];
-workbook.xlsx.readFile(migrateFilePath)
-    .then(function(res) {
-        // console.log(res);
-        const worksheet = workbook.getWorksheet(1);
-        for (let startRow = documentRowStart; true; startRow++) {
-            const rowValues = worksheet.getRow(startRow).values;
-            const val = Model.convertValuesByModel(rowValues, 'masterlokasiujiannonregular');            
-            if (val.documentkey === undefined) break;
-            records.push(val);
-        }
+async function migarateExcel (modelName) {
+    let migrateFilePath;
+    let documentRowStart;
+    let sheetId;
+    let recordToRead;
+    switch (modelName) {
+        case 'masterlokasiujiannonregular':
+            migrateFilePath = './data/MasterLokasiUjianOnlineNonReguler.xlsx';
+            documentRowStart = 6;
+            recordToRead = 358;
+            sheetId = 1;
+            break;
+        case 'masternomorrekening':        
+            migrateFilePath = './data/MasterNomorRekening2.xlsx';
+            documentRowStart = 7;
+            recordToRead = 164;
+            sheetId = 'Sheet1';
+            break;
+        case 'masterlokasiujiandanperjanjiankerjasama':
+            migrateFilePath = './data/MasterLokasiUjiandanPerjanjianKerjasama.xlsx';
+            documentRowStart = 6;
+            recordToRead = 164;
+            break;
+        default:
+            break;
+    }
+    
+    console.log('Loading...')
+    const masterRecord = await Model.getMasterRecord(modelName);
+    var workbook = new Excel.Workbook();
+    const records = [];
+    let queryInsert;
+    
+    await workbook.xlsx.readFile(migrateFilePath)
+        .then(function(res) {
+            // workbook.eachSheet(function(worksheet, sheetId) {
+            //     console.log(sheetId);
+            // })
+
+            const worksheet = workbook.getWorksheet(sheetId);
+            const limit = documentRowStart + recordToRead;
+            for (let startRow = documentRowStart; startRow !== limit; startRow++) {
+                const rowValues = worksheet.getRow(startRow).values;
+                const value = Model.convertValuesByModel(rowValues, modelName);                
+                const valueToInsert = Model.getRecordToInsert(value, masterRecord, modelName);
+                records.push(valueToInsert);
+            }
+            console.log(`${records.length} record Readed`);
+
+            queryInsert = Model.getQueryInsert(modelName, records);
+        });
+
+    const response = {
+        report: global.response,
+        record: records
+    }
+
+    fs.writeFile('./query.sql', String(queryInsert), 'utf8', (err, success) => {
+        console.log("Query written");
     });
 
-    
- 
-// pipe from stream
-// var workbook = new Excel.Workbook();
-// stream.pipe(workbook.xlsx.createInputStream());
+    fs.writeFile('./response.json', JSON.stringify(response, null, 4), 'utf8', (err, success) => {
+        console.log("Response written");
+        process.exit()
+    });
+}
 
-
-
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'me',
-//   password : 'secret',
-//   database : 'my_db'
-// });
- 
-// connection.connect();
- 
-// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-//   if (error) throw error;
-//   console.log('The solution is: ', results[0].solution);
-// });
- 
-// connection.end();
+/**
+ * Execute by params below:
+ * masterlokasiujiannonregular
+ * masternomorrekening
+ * masterlokasiujiandanperjanjiankerjasama    
+ */
+migarateExcel('masterlokasiujiannonregular');
